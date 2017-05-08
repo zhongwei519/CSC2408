97a0181bd4868ad6d8455fd6d26c1ef0d788f1d6
Zhongwei Zhang <zhongwei@usq.edu.au>
2017-05-08 22:57:33 +1000

    Add my_strlen function

--- hello.c
+++ hello.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main()
+
+int my_strlen(char *s)
 {
-     printf("Hello World\n");
-     
+    char *p = s;
+    
+    while (*p)
+      ++p;
+
+    return (p - s);
+}
+
+int main(void)
+{
+     int i;
+
+     char *s[] = {
+          "Git tutorials",
+          "Tutorials Point"
+        };
+
+     for (i = 0; i < 2; ++i)
+         printf("string length of %s = %d\n", s[i], my_strlen(s[i]));
+
      return 0;
 }
